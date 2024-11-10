13f1948e6bfb67fcda6b41050b03247a660a2830
Eli Pousson <eli.pousson@gmail.com>
2024-11-10 16:57:57 -0500

    Add cli to Imports

--- R/openxlsx2Extras-package.R
+++ R/openxlsx2Extras-package.R
@@ -4,6 +4,7 @@
 ## usethis namespace: start
 #' @import rlang
 #' @importFrom openxlsx2 current_sheet
-#' @importFrom lifecycle deprecated
+#' @importFrom lifecycle deprecated badge
+#' @importFrom cli cli_abort cli_warn
 ## usethis namespace: end
 NULL
