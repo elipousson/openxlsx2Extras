6144cbb2ce73347dbe5e20c690dc6a8b0ad09896
Eli Pousson <eli.pousson@gmail.com>
2024-11-10 16:57:14 -0500

    Export `read_xlsx_ext()`
    Also add description

--- R/read_xlsx_ext.R
+++ R/read_xlsx_ext.R
@@ -1,7 +1,16 @@
 #' Create a data frame from a Workbook (with extra features)
 #'
+#' @description
+#' `r lifecycle::badge("experimental")`
+#'
+#' [read_xlsx_ext()] uses [openxlsx2::read_xlsx()] but allows use of a name
+#' repair argument (`"unique"` by default) to avoid blank `""` or `NA` values
+#' for column names.
+#'
+#' @inheritParams openxlsx2::read_xlsx
 #' @inheritParams vctrs::vec_as_names
 #' @inheritDotParams openxlsx2::read_xlsx
+#' @export
 read_xlsx_ext <- function(file,
                           ...,
                           repair = "unique") {
