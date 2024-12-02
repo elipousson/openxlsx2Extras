43bdd3aaa663255e469ffcd5647334f6ed67df75
Eli Pousson <eli.pousson@gmail.com>
2024-12-02 15:50:02 -0500

    Add dplyr and tidyselect to Suggests
    Also fix docs for `fmt_lgl_cols()` and add note to `vec_fmt_lgl()` re: alternate simplified approach

--- R/fmt_lgl_cols.R
+++ R/fmt_lgl_cols.R
@@ -9,6 +9,10 @@
 #' vec_fmt_lgl(c(TRUE, FALSE), c("Yes", "No"))
 #' @export
 vec_fmt_lgl <- function(x, values = c("Y", "N")) {
+  check_installed("dplyr")
+  # TODO: Explore simplified approach suggested by @JanMarvin (and avoid dplyr and tidyselect dependencies)
+  # https://github.com/elipousson/openxlsx2Extras/commit/0a06c7d964dfeabee6f530ff8b01d22a813fad6e#commitcomment-149468724
+
   stopifnot(rlang::has_length(values, 2))
 
   dplyr::if_else(
@@ -23,6 +27,7 @@ vec_fmt_lgl <- function(x, values = c("Y", "N")) {
 #' [fmt_lgl_cols()] uses [vec_fmt_lgl()] to format all (or specified) logical
 #' vector columns to use replacement values.
 #'
+#' @inheritParams dplyr::mutate
 #' @inheritParams dplyr::across
 #' @inheritParams vec_fmt_lgl
 #' @examples
@@ -31,6 +36,7 @@ vec_fmt_lgl <- function(x, values = c("Y", "N")) {
 fmt_lgl_cols <- function(.data,
                          .cols = tidyselect::where(is.logical),
                          values = c("Y", "N")) {
+  check_installed("dplyr")
   .data |>
     dplyr::mutate(
       dplyr::across(
