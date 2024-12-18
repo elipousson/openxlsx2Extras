14f6ba65b6e3dc42ceb11e31046ac767a5affaa2
Eli Pousson <eli.pousson@gmail.com>
2024-12-18 13:08:52 -0500

    Add examples to wb_set_col_widths_ext

--- R/wb_set_col_widths_ext.R
+++ R/wb_set_col_widths_ext.R
@@ -2,11 +2,19 @@
 #'
 #' [wb_set_col_widths_ext()] extends [openxlsx2::wb_set_col_widths()] by making
 #' `cols` optional (defaults to all columns) and allows setting columns based on
-#' column names.
+#' column names (rather than index position alone).
 #'
 #' @inheritParams openxlsx2::wb_set_col_widths
 #' @seealso
 #'  [openxlsx2::wb_data()], [openxlsx2::col_widths-wb()]
+#' @examples
+#' wb <- as_wb(mtcars)
+#'
+#' wb_set_col_widths_ext(wb, cols = "drat")
+#'
+#' # `cols` is optional
+#' wb_set_col_widths_ext(wb)
+#'
 #' @keywords internal
 #' @export
 #' @importFrom openxlsx2 wb_data wb_set_col_widths
@@ -14,9 +22,9 @@ wb_set_col_widths_ext <- function(wb,
                                   cols = NULL,
                                   widths = "auto",
                                   sheet = current_sheet()) {
-  cols <- cols %||% seq(ncol(openxlsx2::wb_data(wb, sheet)))
+  cols <- cols %||% seq(ncol(openxlsx2::wb_data(wb, sheet = sheet)))
 
-  if (any(is.na(as.integer(cols)))) {
+  if (any(is.na(suppressWarnings(as.integer(cols))))) {
     cols <- wb_cols_to_index(
       wb,
       cols = cols,
