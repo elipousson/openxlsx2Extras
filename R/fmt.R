6aa1b7a72de30cd2951587c74ab194b2c0d715fd
Eli Pousson <eli.pousson@gmail.com>
2025-03-11 10:32:46 -0400

    Fix handling of missing columns by set_excel_fmt_class

--- R/fmt.R
+++ R/fmt.R
@@ -13,21 +13,41 @@
 #'   "hyperlink", "percentage", "scientific", "formula"). Length is recycled to
 #'   match length of cols using [vctrs::vec_recycle()].
 #' @inheritParams rlang::arg_match
+#' @param strict If `TRUE`, error if any character values in cols are not
+#' included in the names of `data`. Default `FALSE`.
 #' @export
 #' @importFrom vctrs vec_recycle
-set_excel_fmt_class <- function(data,
-                                cols,
-                                fmt_class = "currency",
-                                multiple = TRUE) {
+set_excel_fmt_class <- function(
+  data,
+  cols,
+  fmt_class = "currency",
+  multiple = TRUE,
+  strict = FALSE
+) {
+  # Validate format class
   fmt_class <- arg_match(
     fmt_class,
     c(
-      "currency", "accounting", "hyperlink",
-      "percentage", "scientific", "formula"
+      "currency",
+      "accounting",
+      "hyperlink",
+      "percentage",
+      "scientific",
+      "formula"
     ),
     multiple = multiple
   )
 
+  # Drop columns that don't appear in data frame (character input only)
+  if (!strict && is.character(cols)) {
+    cols <- intersect(cols, names(data))
+  } else if (is.character(cols)) {
+    stopifnot(
+      all(has_name(data, cols))
+    )
+  }
+
+  # Apply format class to each specified column
   fmt_class <- vctrs::vec_recycle(fmt_class, size = length(cols))
 
   for (i in seq_along(cols)) {
