73b28b126f7994982be399384fe08d0c98d157c4
Eli Pousson <eli.pousson@gmail.com>
2025-10-20 16:57:39 -0400

    Add wb_protect_worksheets function

---
+++ R/wb_protect.R
@@ -0,0 +1,34 @@
+#' Protect one or more sheets in a workbook
+#'
+#' @param properties Defaults to `c("insertColumns", "insertRows",
+#' "deleteColumns", "deleteRows")`
+#' @inheritParams openxlsx2::wb_protect_worksheet
+#' @export
+wb_protect_worksheets <- function(
+  wb,
+  sheet = NULL,
+  protect = TRUE,
+  password = NULL,
+  properties = c(
+          "insertColumns",
+          "insertRows",
+          "deleteColumns",
+          "deleteRows"
+        )
+) {
+  sheet <- sheet %||% wb$sheet_names
+
+  purrr::reduce(
+    sheet,
+    \(x, y) {
+    openxlsx2::wb_protect_worksheet(
+      wb = x,
+      sheet = y,
+      properties = properties,
+      protect = protect,
+      password = password
+    )
+    },
+    .init = wb
+  )
+}
