f841e81e2ecd7008e401a2d14b31afebb70f8240
Eli Pousson <eli.pousson@gmail.com>
2025-10-20 23:21:53 -0400

    Style w/ Air

--- R/wb_protect.R
+++ R/wb_protect.R
@@ -10,24 +10,24 @@ wb_protect_worksheets <- function(
   protect = TRUE,
   password = NULL,
   properties = c(
-          "insertColumns",
-          "insertRows",
-          "deleteColumns",
-          "deleteRows"
-        )
+    "insertColumns",
+    "insertRows",
+    "deleteColumns",
+    "deleteRows"
+  )
 ) {
   sheet <- sheet %||% wb$sheet_names
 
   purrr::reduce(
     sheet,
     \(x, y) {
-    openxlsx2::wb_protect_worksheet(
-      wb = x,
-      sheet = y,
-      properties = properties,
-      protect = protect,
-      password = password
-    )
+      openxlsx2::wb_protect_worksheet(
+        wb = x,
+        sheet = y,
+        properties = properties,
+        protect = protect,
+        password = password
+      )
     },
     .init = wb
   )
